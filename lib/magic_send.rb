a9e291a942b27d0cc19d948761f734e452732d6d
Jon Phenow <j.phenow@gmail.com>
2013-02-26 16:09:33 -0600

    fix

--- lib/magic_send.rb
+++ lib/magic_send.rb
@@ -1,5 +1,57 @@
 require "magic_send/version"
 
+class Object
+  def magic_send(*args)
+    MagicSend::MagicSender.magic self, args
+  end
+end
+
 module MagicSend
-  # Your code goes here...
+  class MagicSender
+    def self.magic(obj, args)
+      new(obj, args).wave_wand
+    end
+
+    def initialize(object, args)
+      @object = object
+      @args = args
+      @tries = 0
+      get_method
+    end
+
+    def wave_wand
+      call
+    end
+
+    private
+
+    def call
+      too_many? ? blow_up : @object.send(@method)
+    rescue ArgumentError
+      try_again
+      call
+    end
+
+    def too_many?
+      @tries > 200
+    end
+
+    def blow_up
+      puts "Your magic needs some work. Please try again."
+    end
+
+    def try_again
+      @method = nil
+      @tries += 1
+      get_method
+    end
+
+    def get_method
+      @method ||= @object.methods.sample
+    end
+
+    def arity
+      @method.arity
+    end
+  end
 end
