da66897818f6179dbc50200e981db28d2372d123
Jon Phenow <j.phenow@gmail.com>
2013-02-26 16:20:12 -0600

    cleanup the object, because this matters

--- lib/magic_send.rb
+++ lib/magic_send.rb
@@ -8,50 +8,60 @@ end
 
 module MagicSend
   class MagicSender
+    private
+    attr_accessor :object
+    attr_accessor :tries
+    attr_accessor :args
+
+    public
+
     def self.magic(obj, args)
       new(obj, args).wave_wand
     end
 
     def initialize(object, args)
-      @object = object
-      @args = args
-      @tries = 0
-      get_method
+      self.object = object
+      self.args = args
+      self.tries = 0
+      self.method = get_method
     end
 
     def wave_wand
       call
     end
 
-    private
-
     def call
-      too_many? ? blow_up : @object.send(@method)
+      too_many? ? blow_up : object.send(method, *args)
     rescue ArgumentError
       try_again
       call
     end
+    private :call
 
     def too_many?
-      @tries > 200
+      tries > 200
     end
+    private :too_many?
 
     def blow_up
       puts "Your magic needs some work. Please try again."
     end
+    private :blow_up
 
     def try_again
-      @method = nil
-      @tries += 1
-      get_method
+      self.tries += 1
+      self.method = get_method
     end
+    private :try_again
 
     def get_method
-      @method ||= @object.methods.sample
+      object.methods.sample
     end
+    private
 
     def arity
-      @method.arity
+      method.arity
     end
+    private :arity
   end
 end
